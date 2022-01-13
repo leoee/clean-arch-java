da40479d2d649b5c3999bb061fae6593dbd7a05e
leoee <leonardoe1997@gmail.com>
2022-01-13 10:25:54 -0300

    Remove unused code

--- src/main/java/com/demo/clean_architecture/presentation/rest/student/StudentResource.java
+++ src/main/java/com/demo/clean_architecture/presentation/rest/student/StudentResource.java
@@ -7,7 +7,6 @@ import com.demo.clean_architecture.usecases.student.AddStudent;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,11 +27,6 @@ public class StudentResource {
   ResponseEntity<?> createStudent(@RequestBody StudentRequest studentRequest) {
 
     return new ResponseEntity<>(this.student.addStudent(StudentInputMapper.map(studentRequest)), HttpStatus.CREATED);
-  }
-
-  @GetMapping("/hello")
-  public String add() {
-      return "Hello";
   }
 
 }
